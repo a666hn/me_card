c36c4b3e4d7384181f6b73163ad3a584134071be
Adi Hermawan <adi.hermawan@sinbad.co.id>
2021-12-15 22:04:23 +0700

    Init repository

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeCard());
+}
+
+class MeCard extends StatelessWidget {
+  const MeCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: const Text('Fuck You'),
+          ),
+        ),
+      ),
+    );
+  }
+}
